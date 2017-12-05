c99dfdac0bb4b85f3e0f3a0a3675a95c29071e61
John Ferlito <johnf@inodes.org>
2017-12-05 17:51:54 +1100

    Remove all the thing shadow code

--- src/index.js
+++ src/index.js
@@ -3,40 +3,13 @@ import log from 'electron-log';
 
 import rootCertificatePem from './caCert';
 
-let thingShadows;
-let thingName;
-
-const saveState = (name, value) => {
-  const state = {
-    state: {
-      reported: {},
-    },
-  };
-
-  // TODO load current state and merge
-
-  state.state.reported[name] = value;
-
-  const clientTokenUpdate = thingShadows.update(thingName, state);
-
-  if (clientTokenUpdate === null) {
-    console.error('update shadow failed, operation still in progress');
-  }
-
-  log.debug(state);
-  log.debug(`saved state for ${name}`);
-
-  // TODO keep track of clientTokenUpdate and updates
-  // TODO: Retry on fail?
-};
-
 const setup = (options, imports, register) => {
   if (!options.x509) {
     register('Not configured');
     return;
   }
 
-  thingName = options.clientId;
+  const thingName = options.clientId;
 
   const iotOptions = {
     clientId: options.clientId,
@@ -46,59 +19,36 @@ const setup = (options, imports, register) => {
     host: options.endpoint,
   };
 
-  thingShadows = iot.thingShadow(iotOptions);
+  const device = iot.device(iotOptions);
 
-  thingShadows.on('connect', () => {
+  device.on('connect', () => {
     log.debug('connect');
 
-    log.debug(`register ${thingName}`);
-    thingShadows.register(thingName, {}, (err, failedTopics) => {
-      if (err) {
-        log.debug('Failed to register: ', err);
-        log.debug('Failed topics: ', failedTopics);
-
-        register('Failed to register');
-        return;
-      }
-
-      log.debug('registration successful');
-
-      register(null, {
-        iot: {
-          saveState,
-          thingShadows,
-        },
-      });
+    register(null, {
+      iot: {
+        device,
+      },
     });
 
     log.debug(`subscribe to hubber/${thingName}`);
-    thingShadows.subscribe(`hubber/${thingName}/#`);
+    device.subscribe(`hubber/${thingName}/#`);
   });
 
   // All below just for debugging
-
-  thingShadows.on('status', (name, stat, clientToken, stateObject) => {
-    log.debug(`received status ${stat} from ${clientToken} on ${name}: ${JSON.stringify(stateObject)}`);
-  });
-
-  thingShadows.on('delta', (name, stateObject) => {
-    log.debug(`received delta on ${name}: ${JSON.stringify(stateObject)}`);
-  });
-
-  thingShadows.on('timeout', (name, clientToken) => {
-    log.debug(`received timeout on ${name} with token: ${clientToken}`);
-  });
-
-  thingShadows.on('reconnect', () => {
+  device.on('reconnect', () => {
     log.debug('reconnect');
   });
 
-  thingShadows.on('close', () => {
+  device.on('close', () => {
     log.debug('close');
   });
 
-  thingShadows.on('offline', () => {
+  device.on('offline', () => {
     log.debug('offline');
+  });
+
+  device.on('error', (error) => {
+    log.debug('error', error);
   });
 };
 
